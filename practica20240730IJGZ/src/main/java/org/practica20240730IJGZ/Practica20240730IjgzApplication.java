c03ad4f54c4b7074c8e68f934816d1dfe19ff5ff
IGabriel30 <ingridgabriel30@hotmail.com>
2024-07-30 12:26:10 -0600

    creación del proyecto

---
+++ practica20240730IJGZ/src/main/java/org/practica20240730IJGZ/Practica20240730IjgzApplication.java
@@ -0,0 +1,13 @@
+package org.practica20240730IJGZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica20240730IjgzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica20240730IjgzApplication.class, args);
+	}
+
+}
---
+++ practica20240730IJGZ/src/test/java/org/practica20240730IJGZ/Practica20240730IjgzApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practica20240730IJGZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica20240730IjgzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
