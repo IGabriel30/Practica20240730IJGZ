ba6d6e84d3d961c8ba4a82146351e341e31fb877
IGabriel30 <ingridgabriel30@hotmail.com>
2024-07-30 21:00:01 -0600

    Estructura del proyecto y configuracion del layout

---
+++ practica20240730IJGZ/src/main/java/org/practica20240730IJGZ/controladores/HomeController.java
@@ -0,0 +1,15 @@
+package org.practica20240730IJGZ.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String index(){
+        return "home/index";
+    }
+}
